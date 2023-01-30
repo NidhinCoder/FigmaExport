9400b75a541b6f44bab0b1c1eff359bea03e8ec1
nkuttikk <nkuttikk@jaguarlandrover.com>
2023-01-30 10:50:18 +0000

    code.ts updated to make a selection
    manifest.json updated
    
    tsconfig.json updated

--- DIN-5808-Sale1.0/code.js
+++ DIN-5808-Sale1.0/code.js
@@ -1,30 +1,42 @@
-"use strict";
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-    }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+const { selection } = figma.currentPage;
+function hasValidSelection(nodes) {
+    return !(!nodes || nodes.length === 0);
+}
+function main(nodes) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!hasValidSelection(selection))
+            return Promise.resolve("Nothing selected for export");
+        let exportableBytes = [];
+        for (let node of nodes) {
+            let { name, exportSettings } = node;
+            if (exportSettings.length === 0) {
+                exportSettings = [{ format: "PNG", suffix: '', constraint: { type: "SCALE", value: 1 }, contentsOnly: true }];
+            }
+            for (let setting of exportSettings) {
+                let defaultSetting = setting;
+                const bytes = yield node.exportAsync(defaultSetting);
+                exportableBytes.push({
+                    name,
+                    setting,
+                    bytes,
+                });
+            }
+        }
+        figma.showUI(__html__, { visible: false });
+        figma.ui.postMessage({ exportableBytes });
+        return new Promise(res => {
+            figma.ui.onmessage = () => res;
+        });
+    });
+}
+main(selection).then(res => figma.closePlugin(res));
+export {};
